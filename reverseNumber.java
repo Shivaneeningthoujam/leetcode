fc71bf587b2b6d9f227d083e4cad4463fdf8afcb
Shivaneeningthoujam <shivaneeningthoujam31@gmail.com>
2024-06-11 09:37:48 +0530

    Reversing a given number

---
+++ reverseNumber.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class reverseNumber {
+    public static void revNum(int num) {
+        // Reverse the number to extract digits in the correct order
+        int rev = 0;
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        // Extract digits from reversed number
+        while (rev > 0) {
+            int digit = rev % 10;
+            System.out.println(digit);
+            rev /= 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 12345;
+        revNum(number);
+    }
+
+}
